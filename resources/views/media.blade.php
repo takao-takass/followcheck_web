e414aa459c068bc95a3f41a4b30c2b0ae2c0694e
多田昂央 <takass0119@yahoo.co.jp>
2020-11-17 22:26:35 +0900

    wip

--- resources/views/media.blade.php
+++ resources/views/media.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
 
-    <a href="{{ $Media->path }}"><img src="{{ $Media->path }}" style="max-width:99vw;max-height:99vh;" /></a>
+    <a href="{{ $Media->path }}"><img src="{{ $Media->path }}" style="max-width:100vw;max-height:100vh;" /></a>
 
     <!-- メインコンテンツ -->
     <div class="container">
