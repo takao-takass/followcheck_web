1acc0154f872efc1c4c496b33aaa4527eb7ac349
TakahiroTada <t.tada@kms3.com>
2020-12-31 16:05:48 +0900

    bugfix

--- resources/views/show_keep.blade.php
+++ resources/views/show_keep.blade.php
@@ -6,6 +6,7 @@
 
 @section('style')
     <link rel="stylesheet" href="{{ asset('/css/show.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/darktheme.css') }}">
 @endsection
 
 @section('content')
