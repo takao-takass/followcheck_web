66791d9d30d6cc172d6c689aa20abc66479c14c4
多田昂央 <takass0119@yahoo.co.jp>
2021-03-21 01:32:43 +0900

    wip
    #23

--- public/js/app.js
+++ public/js/app.js
@@ -9,9 +9,10 @@ function page(num){
 function asyncLoad(){
     $('.async-load').each(function(i){
         if(this.attributes["data-async-load"]!=''){
+            var elm = this;
             var uri = this.attributes['data-async-load'].value;
             $.get(uri ,function(data){
-                this.attributes["src"] = uri;
+                elm.setAttribute("src", uri);
             });
         }
     });
