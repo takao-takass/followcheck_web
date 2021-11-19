2b91998a16c46e318c2b41cab6167534e5c5b9e9
多田昂央 <takass0119@yahoo.co.jp>
2021-11-20 03:49:55 +0900

    ユーザ最新化バッチをPHPに移植。

--- app/Constants/ApiRoute.php
+++ app/Constants/ApiRoute.php
@@ -5,4 +5,5 @@ namespace App\Constants;
 class ApiRoute
 {
     const BATCH_USER_REPAIR = 'batch.user_repair';
+    const BATCH_UPDATE_USERS = 'batch.update_users';
 }
---
+++ app/Constants/LogClasses.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Constants;
+
+class LogClasses
+{
+    const START = 'start';
+    const END = 'end';
+    const INFO = 'info';
+    const WARNING = 'warning';
+    const ERROR = 'error';
+}
